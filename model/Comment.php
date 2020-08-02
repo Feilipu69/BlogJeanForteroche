cd89b56ebf0034b90c6ff2191521477becfa91c1
Feilipu <Turgon.Telrunya@protonmail.com>
2020-08-02 10:59:15 +0200

    Création des models Episode et Comment

---
+++ model/Comment.php
@@ -0,0 +1,74 @@
+<?php
+class Comment
+{
+	private $id;
+	private $author;
+	private $episodeId;
+	private $comment;
+	private $dateComment;
+	private $rudeComment;
+
+	public function __construct(array $datas){
+		$this->hydrate($datas);
+	}
+
+	public function hydrate(array $datas){
+		foreach ($datas as $key => $value) {
+			$method = 'set' . ucfirst($key);
+			if (method_exists($this, $method)) {
+				$this->method($value);
+			}
+		}
+	}
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function getAuthor(){
+		return $this->author;
+	}
+
+	public function getEpisodeId(){
+		return $this->episodeId;
+	}
+
+	public function getComment(){
+		return $this->comment;
+	}
+
+	public function getDateComment(){
+		return $this->dateComment;
+	}
+
+	public function getRudeComment(){
+		return $this->rudeComment;
+	}
+
+	public function setId(int $id){
+		if ($id > 0) {
+			$this->id = $id;
+		}
+	}
+
+	public function setAuthor(string $author){
+		$this->author = $author;
+	}
+
+	public function setEpisodeId(int $episodeId){
+		$this->episodeId = $episodeId;
+	}
+
+	public function setComment(string $comment){
+		$this->comment = $comment;
+	}
+
+	public function setDateComment($dateComment){
+		$this->dateComment = $dateComment;
+	}
+
+	public function setRudeComment(string $rudeComment){
+		$this->rudeComment = $rudeComment;
+	}
+}
---
+++ model/Episode.php
@@ -0,0 +1,84 @@
+<?php
+class Episode
+{
+	private $id;
+	private $chapter;
+	private $title;
+	private $content;
+	private $creationDate;
+	private $updateDate;
+	private $userId;
+
+	public function __construct(array $datas){
+		$this->hydrate($datas);
+	}
+
+	public function hydrate(array $datas){
+		foreach ($datas as $key => $value) {
+			$method = 'set' . ucfirst($key);
+			if (method_exists($this, $method)) {
+				$this->$method($value);
+			}
+		}
+	}
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function getChapter(){
+		return $this->chapter;
+	}
+
+	public function getTitle(){
+		return $this->title;
+	}
+
+	public function getContent(){
+		return $this->content;
+	}
+
+	public function getCreationDate(){
+		return $this->creationDate;
+	}
+
+	public function getUpdateDate(){
+		return $this->updateDate;
+	}
+
+	public function getUserId(){
+		return $this->userId;
+	}
+
+	public function setId(int $id){
+		if ($id > 0) {
+			$this->id = $id;
+		}
+	}
+
+	public function setChapter(string $chapter){
+		$this->chapter = $chapter;
+	}
+
+	public function setTitle(string $title){
+		$this->title = $title;
+	}
+
+	public function setContent(string $content){
+		$this->content = $content;
+	}
+
+	public function setCreationDate($creationDate){
+		$this->creationDate = $creationDate;
+	}
+
+	public function setUpdateDate($updateDate){
+		$this->updateDate = $updateDate;
+	}
+
+	public function setUserId(int $userId){
+		if ($userId > 0) {
+			$this->userId = $userId;
+		}
+	}
+}
